0906387910d066ac550b12c3900b072749df0b9c
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-04-19 11:43:19 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,23 +1,20 @@
 def solution(s):
     answer = len(s)
-    for i in range(1, (len(s)//2)+1):
-        compressed = ""
+    for i in range(1, (len(s) // 2) + 1):
         prev = s[:i]
         cnt = 1
-        for j in range(i, len(s), i):
+        temp = 0
+        for j in range(i, len(s)+i, i):
             curr = s[j:j+i]
             if prev == curr:
                 cnt += 1
             else:
-                if cnt == 1:
-                    compressed += prev
+                if cnt != 1:
+                    temp += len(str(cnt)) + len(prev)
                 else:
-                    compressed += str(cnt) + prev
-                prev = curr
+                    temp += len(prev)
                 cnt = 1
-        if cnt != 1:
-            compressed += str(cnt) + prev
-        else:
-            compressed += prev
-        answer = min(answer, len(compressed))
+                prev = curr
+        answer = min(answer, temp)
+        temp = 0
     return answer
