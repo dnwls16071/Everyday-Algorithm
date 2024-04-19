9bf9eecd2eda3ef75d6373139146ca634a63fb25
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-04-19 12:22:18 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.23 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,24 +1,8 @@
 def solution(s):
-    val1, val2 = convert(s)
-    return [val1, val2]
-
-
-def convert(n):
-    zero_cnt = 0
-    conv_cnt = 0
-    while len(n) != 1:
-        result = ""
-        for i in n:
-            if int(i) == 0:
-                zero_cnt += 1
-            else:
-                result += "1"
-        len_res = len(result)
-        answer = []
-        while len_res != 0:
-            answer.append(len_res % 2)
-            len_res //= 2
-        answer = answer[::-1]
-        n = answer
-        conv_cnt += 1
-    return [conv_cnt, zero_cnt]
+    change, zero = 0, 0
+    while s != '1':
+        change += 1
+        num = s.count('1')
+        zero += len(s) - num
+        s = bin(num)[2:]
+    return [change, zero]
