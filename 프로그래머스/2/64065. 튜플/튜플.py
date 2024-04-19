b9f29375ff29a4f95a3b736634aa85fe04a91d17
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-04-19 10:38:26 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,9 +1,9 @@
 def solution(s):
     s = sorted(s[2:-2].split("},{"), key=lambda x:len(x))
-    result = []
+    dic = dict()
     for i in s:
         data = i.split(",")
         for j in data:
-            if int(j) not in result:
-                result.append(int(j))
-    return result
+            if int(j) not in dic:
+                dic[int(j)] = 1
+    return list(dic)
