8d574b8478f221cec3750ef4ad702d03cf82e1d1
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-05-03 11:04:23 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,6 +1,9 @@
 def solution(brown, yellow):
+    """
+    area : 전체 면적(갈색 타일과 노란색 타일의 합)
+    """
     area = brown + yellow
-    for i in range(1, brown + 1):
+    for i in range(2, brown + 1):
         if area % i == 0:
             a, b = i, area // i
             if (a-2) * (b-2) == yellow:
