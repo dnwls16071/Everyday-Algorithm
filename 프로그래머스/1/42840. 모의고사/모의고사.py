1b0e095d69f6a934f4bb4249dd6b4ac8a4eceef3
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-05-03 10:35:53 +0900

    [level 1] Title: 모의고사, Time: 3.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,5 +1,4 @@
 def solution(answers):
-    MAX = -1
     first = [1, 2, 3, 4, 5]
     second = [2, 1, 2, 3, 2, 4, 2, 5]
     third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]        
@@ -15,16 +14,14 @@ def solution(answers):
         if answers[i] == third[i % len(third)]:
             result[2] += 1
             
-    for i in result:
-        if i > MAX:
-            MAX = i
-            
     for idx, val in enumerate(result):
         answer.append([idx, val]) 
     answer = sorted(answer, key=lambda x : (-x[1], x[0]))
     
     ans = []
     for idx, val in answer:
-        if val == MAX:
+        if val == max(result):
             ans.append(idx+1)
+        else:
+            break
     return ans
