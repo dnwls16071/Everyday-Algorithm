418740c75766d878cb626634a0a3fb376d561922
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-05-06 17:36:10 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers  = sorted(numbers, key=lambda x : x*3, reverse=True)
+    
+    answer = "".join(map(str, numbers))
+    if int(answer) == 0:
+        return "0"
+    else:
+        return "".join(map(str, numbers))
+    
