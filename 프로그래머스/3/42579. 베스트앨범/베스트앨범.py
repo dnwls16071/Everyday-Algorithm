ff09da39c4d4d3e8fe47f9cbc97c2bd0dbf75222
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-05-08 10:41:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,23 +1,22 @@
+from collections import defaultdict
+
 def solution(genres, plays):
+    """
+    노래 수록 기준(장르 별로 2개씩 모아...)
+    1. 속한 노래가 많이 재생된 장르 먼저
+    2. 장르 내에서 많이 재생된 노래 먼저
+    3. 장르 내에서 재생 횟수가 같은 노래 중에서 고유 번호가 낮은 노래를 먼저
+    """
+    info = defaultdict(lambda: [[], [], 0])
     answer = []
-    dic = {}
-    for i, (genre, play) in enumerate(zip(genres, plays)):
-        if genre in dic.keys():
-            dic[genre][0].append(i)      # 고유 번호
-            dic[genre][1].append(play)   # 재생 횟수
-            dic[genre][2] += play        # 장르별 누적 재생 횟수
-        else:
-            dic[genre] = [[i], [play], play]
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        info[genre][0].append(idx)      # 고유 번호
+        info[genre][1].append(play)     # 해당 고유 번호 장르 재생 횟수
+        info[genre][2] += play          # 장르 총 재생 횟수
+    info = sorted(info.items(), key=lambda x : -x[1][2])
     
-    # [1]. 속한 노래가 많이 재생된 장르 먼저 수록 -> 장르별 누적 재생 횟수 기준 내림차순
-    new_dic = sorted(dic.items(), key=lambda x : -x[1][2])
-    
-    # [2]. 장르 내에서 많이 재생된 노래 먼저 수록 -> 재생 횟수 기준 내림차순
-    # [3]. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래 먼저 수록
-    for genre, info in new_dic:
-        # x[0] : 고유 번호
-        # x[1] : 재생 횟수
-        songs = sorted(zip(info[0], info[1]), key=lambda x : (-x[1], x[0]))
-        for index, value in songs[:2]:
-            answer.append(index)
-    return answer 
+    for g, i in info:
+        song = sorted(zip(i[0], i[1]), key=lambda x : (-x[1], x[0]))[:2]
+        for s in song:
+            answer.append(s[0])
+    return answer
