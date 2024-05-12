879292e9ad66a6fc85bcc69044818600ad65627d
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-05-13 08:48:04 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,23 +1,21 @@
 from heapq import heappush as push, heappop as pop
 
 def solution(jobs):
-    answer = 0
-    now = 0
-    idx = 0
+    answer, now, idx = 0, 0, 0
     start = -1
     heap = []
     
     while idx < len(jobs):
         for job in jobs:
             if start < job[0] <= now:
-                push(heap, job[::-1])      
-        
+                push(heap, job[::-1])
+                
         if len(heap) > 0:
-            cur = pop(heap)
+            current = pop(heap)
             start = now
             idx += 1
-            now += cur[0]
-            answer += (now - cur[1])
+            now += current[0] 
+            answer += (now - current[1])            
         else:
             now += 1
     return answer // len(jobs)
