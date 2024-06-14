7fe2900fd6b259fb525229ca48d63658c6a1c443
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-06-14 18:03:07 +0900

    [level 2] Title: [3차] n진수 게임, Time: 23.37 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -2,7 +2,7 @@ def conv(num, n):
     dic = {10 : "A", 11 : "B", 12 : "C", 13 : "D", 14 : "E", 15 : "F"}
     result = []
     if num == 0:
-        return [0]
+        return ''.join(map(str, [0]))
     else:
         while num != 0:
             if num % n >= 10:
@@ -10,7 +10,7 @@ def conv(num, n):
             else:
                 result.append(num % n)
             num //= n
-        return result[::-1]
+        return ''.join(map(str, result[::-1]))
 
 # 진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p
 def solution(n, t, m, p):
@@ -19,8 +19,7 @@ def solution(n, t, m, p):
     for i in range(m * t):
         # i를 n진법으로 변환
         temp = conv(i, n)
-        for j in range(0, len(temp)):
-            game_lst.append(temp[j])
+        game_lst.extend(temp)
     for i in range(p-1, m * t, m):
         result.append(game_lst[i])
     return ''.join(map(str, result))
