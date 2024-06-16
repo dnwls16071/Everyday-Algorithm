81dc466eb85d480c36bc0fd00f7c45bdf325ff8a
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-06-16 10:11:47 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 712 ms, Memory: 159944 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.py
@@ -1,38 +1,43 @@
 import sys, heapq
-from collections import defaultdict
+from collections import defaultdict, deque
 input = sys.stdin.readline
-result = []
 
 N = int(input())
-min_heap = []; max_heap = []
+minH = []; maxH = []
+proHash = defaultdict(list)
+lvHash = defaultdict(int)
 for _ in range(N):
+    # P : 문제 번호, L : 난이도
+    # 가장 어려운 문제 번호 출력을 위해 첫 번째에 난이도, 두 번째에 문제 번호
     P, L = map(int, input().split())
-    # 단일 값이 아닌 값을 힙에 넣으면 첫 번째 원소를 기준으로 힙을 구성한다.
-    # 최소힙 : 문제 번호가 작은 순서대로
-    # 최대힙 : 문제 번호가 큰 순서대로
-    heapq.heappush(min_heap, [L, P])
-    heapq.heappush(max_heap, [-L, -P])    
+    heapq.heappush(minH, [L, P])
+    heapq.heappush(maxH, [-L, -P])
+    proHash[L].append(P)
+    lvHash[P] = L
 
 M = int(input())
-solved = defaultdict(int)   # 같은 문제지만 난이도가 달리 추가될 수 있으므로 bool → int
 for _ in range(M):
-    commands = list(input().split())
-    if commands[0] == "recommend":
-        # 문제 리스트에서 가장 어려운 문제 번호를 출력
-        if commands[1] == "1":
-            # 풀지 않은 문제라면?
-            while solved[abs(max_heap[0][1])] != 0:
-                solved[abs(max_heap[0][1])] -= 1
-                heapq.heappop(max_heap)
-            print(-max_heap[0][1])
-        elif commands[1] == "-1":
-            # 풀지 않은 문제라면?
-            while solved[min_heap[0][1]] != 0:
-                solved[min_heap[0][1]] -= 1
-                heapq.heappop(min_heap)
-            print(min_heap[0][1])
-    elif commands[0] == "add":
-        heapq.heappush(min_heap, [int(commands[2]), int(commands[1])])
-        heapq.heappush(max_heap, [-int(commands[2]), -int(commands[1])])
+    commands = input().split()
+    if commands[0] == "add":
+        P = int(commands[1]); L = int(commands[2])
+        heapq.heappush(minH, [L, P])
+        heapq.heappush(maxH, [-L, -P])
+        proHash[L].append(P)
+        lvHash[P] = L
+    # 문제를 추가 (+),  문제를 해결 (-)
     elif commands[0] == "solved":
-        solved[int(commands[1])] += 1
+        P = int(commands[1])                   # 문제 번호 P
+        lv = lvHash[P]                         # 해당 문제 난이도
+        del proHash[lv][proHash[lv].index(P)]  # 문제에서 lv에 해당하는 레벨 문제 중에서 P번 문제를 제거
+        lvHash[P] = None
+    elif commands[0] == "recommend":
+        if commands[1] == "1":
+            for lv in range(100, 0, -1):
+                if len(proHash[lv]):
+                    print(max(proHash[lv]))
+                    break
+        elif commands[1] == "-1":
+            for lv in range(1, 101):
+                if len(proHash[lv]):
+                    print(min(proHash[lv]))
+                    break
