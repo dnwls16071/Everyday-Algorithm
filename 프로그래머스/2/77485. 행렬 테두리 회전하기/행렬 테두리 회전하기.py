2dd20b9bf92cfdd3a2aa6018b6f9dfa579c04e05
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-17 09:50:41 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 267.52 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,41 +1,37 @@
-def rotate(graph, x1, y1, x2, y2):
-    min_value = graph[x1][y1]
-    first = min_value
-
-    # 왼쪽 면
-    for i in range(x1, x2):
-        graph[i][y1] = graph[i+1][y1]
-        min_value = min(min_value, graph[i+1][y1])
-
-    # 아래쪽 면
-    for j in range(y1, y2):
-        graph[x2][j] = graph[x2][j+1]
-        min_value = min(min_value, graph[x2][j+1])
-
-    # 오른쪽 면
-    for k in range(x2, x1, -1):
-        graph[k][y2] = graph[k-1][y2]
-        min_value = min(min_value, graph[k-1][y2])
-
-    # 위쪽 면
-    for m in range(y2, y1+1,-1):
-        graph[x1][m] = graph[x1][m-1]
-        min_value = min(min_value, graph[x1][m-1])
-
-    graph[x1][y1+1] = first
-    return min_value
-        
 def solution(rows, columns, queries):
     answer = []
-    graph = [[0] * (columns) for _ in range(rows)]
-    ele = 1
-    for i in range(rows):
-        for j in range(columns):
-            graph[i][j] = ele
-            ele+=1
-
+    value = 1
+    array = [[0] * columns for _ in range(rows)]
+    # 2차원 배열 선언 후 초기화
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            array[i][j] = value
+            value += 1
+    
+    # 회전 순서 : 왼쪽 면 → 아래쪽 면 → 오른쪽 면 → 위쪽 면
+    # 좌상단 값은 따로 변수에 저장 후 회전 다 돌리면 그 때 넣을 것(그래야 값이 유실되지 않음)
     for query in queries:
         x1, y1, x2, y2 = query
-        result = rotate(graph, x1-1, y1-1, x2-1, y2-1)
-        answer.append(result)    
+        
+        first = array[x1-1][y1-1]
+        min_value = first
+
+        # 왼쪽 면
+        for i in range(x1, x2):
+            array[i-1][y1-1] = array[i][y1-1]
+            min_value = min(min_value, array[i][y1-1])
+        # 아래쪽 면
+        for i in range(y1, y2):
+            array[x2-1][i-1] = array[x2-1][i]
+            min_value = min(min_value, array[x2-1][i])
+        # 오른쪽 면
+        for i in range(x2-1, x1-1, -1):
+            array[i][y2-1] = array[i-1][y2-1]
+            min_value = min(min_value, array[i-1][y2-1])
+        # 위쪽 면
+        for i in range(y2-1, y1, -1):
+            array[x1-1][i] = array[x1-1][i-1]
+            min_value = min(min_value, array[x1-1][i-1])
+        array[x1-1][y1] = first
+        answer.append(min_value)
     return answer
