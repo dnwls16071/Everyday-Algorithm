24468570879a4cd28b875f4d3ced20d2c65bd702
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-18 11:58:13 +0900

    [level 1] Title: 시저 암호, Time: 2.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,10 +1,16 @@
+# a : 97, z : 122, A : 65, Z : 90
 def solution(s, n):
-    s = list(s)
+    result = []
+    lower_list = "abcdefghijklmnopqrstuvwxyz"
+    upper_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
     for i in range(len(s)):
-        if s[i].islower():
-            s[i] = chr((ord(s[i]) + n - ord('a')) % 26 + ord('a'))
-        elif s[i].isupper():
-            s[i] = chr((ord(s[i]) + n - ord('A')) % 26 + ord('A'))
+        if s[i].isupper():
+            idx = (upper_list.find(s[i]) + n) % 26
+            result.append(upper_list[idx])
+        elif s[i].islower():
+            idx = (lower_list.find(s[i]) + n) % 26
+            result.append(lower_list[idx])
         else:
-            s[i] = " "
-    return "".join(map(str, s))        
+            result.append(" ")
+    return "".join(result)
