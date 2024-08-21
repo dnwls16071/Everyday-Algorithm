cac97317618c2b304b4e0f048e60a56f8ca16410
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-21 09:08:11 +0900

    [level 2] Title: 하노이의 탑, Time: 9.07 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,12 +1,23 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+# 맨 아래 원판을 제외한 (n-1)개의 원판을 1번 기둥에서 2번 기둥으로 옮긴다.
+# 맨 아래 원판을 1번 기둥에서 3번 기둥으로 옮긴다.
+# 2번 기둥에 있는 (n-1)개의 원판을 3번 기둥으로 옮긴다.
 def solution(n):
     answer = []
-    def hanoi(n, start, end, mid):
+    
+    # n개의 원반을 start(시작 기둥), mid(경유 기둥), end(도착 기둥)
+    def hanoi(n, start, mid, end, answer):
+        # 1개의 원판은 시작 기둥에서 바로 도착 기둥으로 옮긴다.
         if n == 1:
-            answer.append([start, end])
-            return answer
-        hanoi(n-1, start, mid, end)
+            return answer.append([start, end])
+        # (n-1)개의 원판은 시작 기둥에서 도착 기둥을 경유하여 중간 기둥으로 옮긴다.
+        hanoi(n-1, start, end, mid, answer)
+        # 1개의 원판은 시작 기둥에서 바로 도착 기둥으로 옮긴다.
         answer.append([start, end])
-        hanoi(n-1, mid, end, start)
-        return answer
-    answer = hanoi(n, 1, 3, 2)
+        # (n-1)개의 원판은 중간 기둥에서 시작 기둥을 경유하여 도착 기둥으로 옮긴다.
+        hanoi(n-1, mid, start, end, answer)
+    
+    hanoi(n, 1, 2, 3, answer)
     return answer
