45bfb2f8c13c6070269efbb7fed6813b68335f2c
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-22 10:10:15 +0900

    [level 2] Title: 소수 찾기, Time: 1176.99 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,21 +1,31 @@
-from itertools import permutations
-
-def check(n):
-    if n == 0 or n == 1:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
-    return True
+import sys
+sys.setrecursionlimit(10**6)
 
 def solution(numbers):
-    cnt = 0
-    lst = list(map(str, numbers))
+    prime_set = set()
+    numbers = list(map(int, numbers))
+    visited = [False] * len(numbers)
 
-    res = set()
-    for i in range(1, len(lst) + 1):
-        for perm in permutations(lst, i):
-            num = int("".join(map(str, perm)))
-            if check(num) and num not in res:
-                res.add(num)
-    return len(res)
+    def checking_prime(number):
+        if number == 1 or number == 0:
+            return False
+        for i in range(2, (number // 2) + 1):
+            if number % i == 0:
+                return False
+        return True
+    
+    def backtracking(combination):
+        if combination:
+            num = int("".join(map(str, combination)))
+            if checking_prime(num):
+                prime_set.add(num)
+        for i in range(len(numbers)):
+            if not visited[i]:
+                visited[i] = True
+                combination.append(numbers[i])
+                backtracking(combination)
+                combination.pop()
+                visited[i] = False
+        
+    backtracking([])
+    return len(prime_set)
