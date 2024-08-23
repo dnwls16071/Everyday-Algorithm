7d6397e635b025c84fc8c33635850ea8121b08ae
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-24 07:47:11 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.71 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,19 +1,10 @@
+from itertools import combinations
+
 def solution(numbers):
-    visited = [False] * len(numbers)
     result = set()
-    def DFS(numbers, lst):
-        if len(lst) == 2:
-            result.add(sum(lst))
-            return
-        for i in range(len(numbers)):
-            if not visited[i]:
-                visited[i] = True
-                lst.append(numbers[i])
-                DFS(numbers, lst)
-                visited[i] = False
-                lst.pop()
-        return result
-    result = DFS(numbers, [])
-    result = list(result)
-    result.sort()
+    for num in combinations(numbers, 2):
+        result.add(sum(num))
+    result = sorted(result)
     return result
+
+    
