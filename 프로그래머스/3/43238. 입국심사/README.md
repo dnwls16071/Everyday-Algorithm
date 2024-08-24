3ce86a5ea824c512545e097f6629f5e1d51cfa55
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-24 09:40:18 +0900

    [level 3] Title: 입국심사, Time: 481.48 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.03 ms
+메모리: 14.1 MB, 시간: 481.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 07일 15:40:54
+2024년 08월 24일 09:40:14
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,19 +1,23 @@
+# 구하고자 하는 것 : 모든 사람이 심사를 받는데 걸리는 시간
+# 테스트케이스 기준 : 6명 인원, 최대 60분 소요
+
 def solution(n, times):
-    answer = 0    
-    left = 1
+    left = 0
     right = max(times) * n
+    
     while left <= right:
-        people = 0
         mid = (left + right) // 2
-        for time in times:
-            people += (mid // time)
+        people = 0
         
-            if people >= n:
+        for time in times:
+            people += (mid // time) # 30분동안 7분 주기로 입국심사를 하면 : 4명 입국심사 가능
+            if people > n:
                 break
         
-        if people >= n:
+        # 입국 심사를 받을 수 잇는 인원이 n명보다 적다면? -> 더 많이 받을 수 있도록 늘려줘야함
+        if people < n:
+            left = mid + 1
+        else:
             answer = mid
             right = mid - 1
-        else:
-            left = mid + 1
-    return answer
+    return answer            
