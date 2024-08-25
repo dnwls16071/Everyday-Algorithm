93afa051facf66f06f61c266c7b2744a02127979
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-25 22:29:37 +0900

    [level 2] Title: 순위 검색, Time: 499.96 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,40 +1,29 @@
+from collections import defaultdict
 from itertools import combinations
 from bisect import bisect_left, bisect_right
-from collections import defaultdict
 
 def solution(info, query):
-    result = []
-    """
-    개발언어 : cpp, java, python
-    직군 : backend, frontend
-    경력 : junior, senior
-    소울푸드 : chicken, pizza
-    총 조합의 수 : 3 x 2 x 2 x 2 = 24
-    """
-    hash_info = defaultdict(list)
-    for i in info:
-        c = i.split(" ")[:]
-        score = c.pop()
-        for r in range(5):
-            combs = combinations(range(4), r)
-            for comb in combs:
-                key = c[:]
-                for idx in comb:
-                    key[idx] = "-"
-                hash_info["".join(key)].append(int(score))
-
-    for key in hash_info.keys():
-        hash_info[key].sort()
-                
+    answer = []
+    # 지원자 정보를 기반으로 조합을 산출
+    # 키에는 점수를 제외한 지원자 정보, 값에는 지원자 점수 리스트(선형 탐색X)
+    data = defaultdict(list)    
+    
+    for i in info:  
+        i = i.split(" ")                              # java backend junior pizza
+        score = int(i.pop())                          # 150
+        data["".join(i)].append(score)                # javabackendjuniorpizza
+        for j in range(4):
+            case = list(combinations(i, j))           # 지원자 정보 조합
+            for k in case:
+                data["".join(k)].append(score)
+    
+    for key, value in data.items():
+        data[key].sort()
+    
     for q in query:
-        c = q.replace(" and", "").split(" ")
-        score = int(c.pop())
-        key = "".join(c)
-        if key not in hash_info:
-            result.append(0)
-        else:
-            right_idx = len(hash_info[key])
-            left_idx = bisect_left(hash_info[key], score)
-            result.append(right_idx - left_idx)
-    return result
-            
+        q = q.split(" ")
+        score = int(q.pop())
+        key = "".join(q)
+        key = key.replace("and", "").replace(" ", "").replace("-", "")
+        answer.append(len(data[key]) - bisect_left(data[key], score))
+    return answer
