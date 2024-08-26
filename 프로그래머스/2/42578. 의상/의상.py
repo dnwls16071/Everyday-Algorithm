5ec8cf150ffc94ae9637f8fdf7532bc1999a7fd2
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-26 13:43:49 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,11 @@
 from collections import defaultdict
 
 def solution(clothes):
-    """
-    전체 입을 수 있는 의상의 조합 수에서 아무것도 안 입은 경우에 해당하는 값인 1을 뺀다.
-    """
-    answer = 1
-    dic = defaultdict(int)
-    for c in clothes:
-        dic[c[1]] += 1
+    info = defaultdict(list)
+    for clothe, type in clothes:
+        info[type].append(clothe)
 
-    for key, val in dic.items():
-        answer *= (val + 1)
+    answer = 1
+    for key, value in info.items():
+        answer *= (len(value) + 1)
     return answer - 1
