3eb51e8685a53d397b737c125f2d64847c40dec3
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-26 13:32:20 +0900

    [level 2] Title: 전화번호 목록, Time: 95.88 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,7 @@
 def solution(phone_book):
-    phone_book.sort()
     flag = True
-    for i in range(len(phone_book)-1):
+    phone_book.sort()
+    for i in range(len(phone_book) - 1):
         if phone_book[i+1].startswith(phone_book[i]):
             flag = False
             break
