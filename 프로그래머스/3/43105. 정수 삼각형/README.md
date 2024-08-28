490c21c3253e18c2d3db0e37935e7a51f2231998
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-28 15:35:35 +0900

    [level 3] Title: 정수 삼각형, Time: 48.19 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 19.4 MB, 시간: 48.44 ms
+메모리: 14.7 MB, 시간: 48.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 10일 12:13:10
+2024년 08월 28일 15:35:32
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,13 +1,11 @@
 def solution(triangle):
-    height = len(triangle)
-    dp = [[0] * height for _ in range(height)]
-    dp[0][0] = triangle[0][0]
-    for i in range(1, height):
-        dp[i][0] = dp[i-1][0] + triangle[i][0]
-        dp[i][i] = dp[i-1][i-1] + triangle[i][-1]
-        
-    for i in range(2, height):
-        for j in range(1, i):
-            dp[i][j] = max(dp[i-1][j-1] + triangle[i][j], dp[i-1][j] + triangle[i][j])    
-    answer = max(dp[-1])
-    return answer
+    # 모서리 부분
+    for i in range(len(triangle) - 1):
+        triangle[i+1][0] += triangle[i][0]
+        triangle[i+1][-1] += triangle[i][-1]
+    
+    # 모서리 제외한 가운데 부분
+    for i in range(2, len(triangle)):
+        for j in range(1, len(triangle[i]) - 1):
+            triangle[i][j] = max(triangle[i][j] + triangle[i-1][j-1], triangle[i][j] + triangle[i-1][j])
+    return max(triangle[-1])
