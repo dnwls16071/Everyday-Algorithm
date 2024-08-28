fb0febc5bd4588c8e7ebe5de460ec05b58ffc4f9
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-28 14:59:01 +0900

    [level 3] Title: N으로 표현, Time: 18.39 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,15 +1,15 @@
 def solution(N, number):
-    dp = [set() for _ in range(9)]
-    for i in range(1, 9):
-        dp[i].add(int(str(N) * i))
+    dp = [set() for i in range(9)]  # N을 i번 사용해서 얻을 수 있는 숫자들의 모임 
+    for i in range(1, 9):           
+        dp[i].add(int(str(N) * i))  # N을 i번 사용(기본값)
         for j in range(1, i):
             for k in dp[j]:
-                for l in dp[i-j]:
-                    dp[i].add(k + l)
-                    dp[i].add(k * l)
-                    dp[i].add(k - l)
-                    if l != 0 and k != 0:
-                        dp[i].add(k // l)
+                for t in dp[i-j]:
+                    dp[i].add(k + t)
+                    dp[i].add(k - t)
+                    dp[i].add(k * t)
+                    if t != 0 and k != 0:
+                        dp[i].add(k // t)
         if number in dp[i]:
             return i
-    return -1  
+    return -1
