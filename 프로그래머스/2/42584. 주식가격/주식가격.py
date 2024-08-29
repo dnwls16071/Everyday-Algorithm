122df956b6793185235607c7f27d3f3f0fe67411
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-29 10:46:41 +0900

    [level 2] Title: 주식가격, Time: 150.10 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,9 +1,10 @@
 def solution(prices):
-    stack = [0] # 시점을 저장하는 수단
-    answer = [i for i in range(len(prices)-1, -1, -1)]
-    for i in range(1, len(prices)):
-        while stack and prices[stack[-1]] > prices[i]:
-            seq = stack.pop()
-            answer[seq] = i - seq
-        stack.append(i)
+    answer = []
+    for i in range(len(prices)):
+        cnt = 0
+        for j in range(i+1, len(prices)):
+            cnt += 1
+            if prices[i] > prices[j]:
+                break
+        answer.append(cnt)
     return answer
