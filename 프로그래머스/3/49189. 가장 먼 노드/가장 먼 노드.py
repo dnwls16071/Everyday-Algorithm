06da35b3b8f5ea982251bded0aae0856d9ece6ef
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-30 09:53:35 +0900

    [level 3] Title: 가장 먼 노드, Time: 28.53 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,32 +1,28 @@
 from collections import deque
 
-def BFS(start, graph, n):
-    answer = [0] * (n + 1)
-    q = deque()
-    q.append(start)
-    visited = [False] * (n + 1)
-    visited[1] = True
-    answer[1] = 1
-    while q:
-        v = q.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                answer[i] = answer[v] + 1
-                q.append(i)
-    return answer
-
 def solution(n, edge):
+    route = [0] * (n + 1)
+    route[1] = 1
     graph = [[] for _ in range(n + 1)]
-    for e in edge:
-        a, b = e
+    for v in edge:
+        a, b = v
         graph[a].append(b)
         graph[b].append(a)
-    answer = BFS(1, graph, n)
     
+    def BFS():
+        q = deque()
+        q.append(1)
+        while q:
+            v = q.popleft()
+            for i in graph[v]:
+                if not route[i]:
+                    route[i] = route[v] + 1
+                    q.append(i)
+    
+    BFS()
+    MAX = max(route)
     cnt = 0
-    MAX = max(answer)
-    for i in answer:
+    for i in route:
         if i == MAX:
             cnt += 1
     return cnt
