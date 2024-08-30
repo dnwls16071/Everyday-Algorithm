ac33d1a645842fe9c2a383e55f140042a03dc9f1
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-30 11:18:28 +0900

    [level 3] Title: 길 찾기 게임, Time: 434.62 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,69 +1,59 @@
 import sys
 sys.setrecursionlimit(10**6)
 
+# 주어진 데이터를 이진 트리로 만들 수 있는가?
+# 이진 트리를 만든 후 트리 순회(전위/후위 순회) 코드까지 구현할 수 있는가?
+
+# data : [x축 좌표, y축 좌표]
+# 노드(본인, 왼쪽, 오른쪽)
 class Node:
-    def __init__(self, info):
-        self.x = info[0][0]
-        self.y = info[0][1]
-        self.idx = (info[1] + 1)
+    def __init__(self, data):
+        self.idx = data[1]      # 인덱스
+        self.data = data[0]    # 좌표평면 좌표값
         self.left = None
         self.right = None
-    
-def insert(node, info):
-    if node is None:
-        return Node(info)
-    
-    if info[0][0] < node.x:
-        node.left = insert(node.left, info)
-    else:
-        node.right = insert(node.right, info)
-    return node
 
-def maketree(nodeinfo):
-    nodes = []
-    for idx, node in enumerate(nodeinfo):
-        nodes.append((node, idx))
-    nodes.sort(key=lambda x : x[0][0])
-    nodes.sort(key=lambda x : x[0][1], reverse=True)
-    
-    root = None
-    for node in nodes:
-        root = insert(root, node)
-    return root
+def addNode(root, info):
+    # 루트 노드보다 값이 크면 오른쪽에 배치
+    if root.data[0] < info[0][0]:
+        if not root.right:
+            root.right = Node(info)
+        else:
+            addNode(root.right, info)
+    # 루트 노드보다 값이 작으면 왼쪽에 배치
+    elif root.data[0] > info[0][0]:
+        if not root.left:
+            root.left = Node(info)
+        else:
+            addNode(root.left, info)
+    # 루트 노드와 값이 같은 경우는 없음
 
-def preorder(root):
-    res = []
-    def _preorder(node):
-        if node:
-            res.append(node.idx)
-            _preorder(node.left)
-            _preorder(node.right)
-    _preorder(root)
-    return res
-
-def inorder(root):
-    res = []
-    def _inorder(node):
-        if node:
-            _inorder(node.left)
-            res.append(node.idx)
-            _inorder(node.right)
-    _inorder(root)
-    return res
-
-def postorder(root):
-    res = []
-    def _postorder(node):
-        if node:
-            _postorder(node.left)
-            _postorder(node.right)
-            res.append(node.idx)
-    _postorder(root)
-    return res
-
+def preorder(root, data):
+    if root:
+        data.append(root.idx)
+        preorder(root.left, data)
+        preorder(root.right, data)
+    return data
+        
+        
+def postorder(root, data):
+    if root:
+        postorder(root.left, data)
+        postorder(root.right, data)
+        data.append(root.idx)
+    return data
+        
 def solution(nodeinfo):
-    answer = []
-    root = maketree(nodeinfo)
-    answer.append(preorder(root))
-    answer.append(postorder(root))
-    return answer
+    nodeinfo = [[info, idx+1] for idx, info in enumerate(nodeinfo)]   # 순서(인덱스) 값도 포함
+    nodeinfo = sorted(nodeinfo, key=lambda x : -x[0][1])              # y축 좌표 기준 내림차순 정렬
+    
+    rootNode = Node(nodeinfo[0])    # 루트 노드 설정
+    
+    for info in nodeinfo[1:]:
+        addNode(rootNode, info)
+
+    pre_lst = preorder(rootNode, [])
+    post_lst = postorder(rootNode, [])
+    return [pre_lst, post_lst]
+    
+    
