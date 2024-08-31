d4fc8d546ca70db76770d8f343d77d30b669b0aa
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-31 13:18:25 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 10.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 10.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 13일 08:48:03
+2024년 08월 31일 13:18:20
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,21 +1,25 @@
-from heapq import heappush as push, heappop as pop
+import heapq
 
 def solution(jobs):
-    answer, now, idx = 0, 0, 0
-    start = -1
-    heap = []
-    
-    while idx < len(jobs):
+    start = -1  # 시작 시간
+    time = 0    # 걸린 시간
+    now = 0     # 현재 시점
+    cnt = 0     # 작업 처리 개수
+    pq = []     # 우선순위 큐
+
+    # [작업 소요시간, 작업 요청 시점]
+    while cnt < len(jobs):
         for job in jobs:
             if start < job[0] <= now:
-                push(heap, job[::-1])
-                
-        if len(heap) > 0:
-            current = pop(heap)
+                heapq.heappush(pq, job[::-1])
+        
+        # 우선순위 큐에 작업이 들어오면?
+        if len(pq) > 0:
+            cur = heapq.heappop(pq)
             start = now
-            idx += 1
-            now += current[0] 
-            answer += (now - current[1])            
+            now += cur[0]
+            time += (now - cur[1])
+            cnt += 1
         else:
             now += 1
-    return answer // len(jobs)
+    return time // len(jobs)
