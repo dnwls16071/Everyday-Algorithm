1d80e38b8e316b5c10a060a82c71494c1082a5bc
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-08-31 23:05:15 +0900

    [level 4] Title: 가사 검색, Time: 5252.21 ms, Memory: 785 MB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/가사 검색.py
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.py
@@ -1,23 +1,56 @@
-from bisect import bisect_left, bisect_right
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.match = 0  # 여기서 중요한 것은 ?가 글자 하나에 매칭이 되므로 문자열의 길이가 일치 여부보다도 중요
+        self.children = {}
 
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+    
+    def insert(self, string):
+        curr = self.head
+        
+        for char in string:
+            curr.match += 1
+            if char not in curr.children:
+                curr.children[char] = Node(char)
+            curr = curr.children[char]
+        
+    def startswith(self, string):
+        curr = self.head
+        
+        for char in string:
+            if char == "?":
+                break
+            if char in curr.children:
+                curr = curr.children[char]
+            else:
+                return 0
+        return curr.match    
+    
 def solution(words, queries):
     answer = []
-    prefix = [[] for _ in range(10001)]
-    suffix = [[] for _ in range(10001)]
+    tries = {}
+    reverse_tries = {}
+    
     for word in words:
-        prefix[len(word)].append(word)
-        suffix[len(word)].append(word[::-1])        
-    for i in range(10001):
-        prefix[i].sort()
-        suffix[i].sort()
-    for q in queries:
-        # 접미사로 ?이 등장하는 경우
-        if q[0] != "?":
-            left_idx = bisect_left(prefix[len(q)], q.replace("?", "a"))
-            right_idx = bisect_right(prefix[len(q)], q.replace("?", "z"))
-        # 접두사로 ?이 등장하는 경우
+        size = len(word)
+        if size not in tries:
+            tries[size] = Trie() # 단어 길이별로 트라이 자료구조를 구축
+            reverse_tries[size] = Trie()
+        tries[size].insert(word)
+        reverse_tries[size].insert(word[::-1])
+    
+    for query in queries:
+        size = len(query)
+        if size in tries:
+            if query[0] != "?":
+                trie = tries[size]
+                answer.append(trie.startswith(query))
+            else:
+                trie = reverse_tries[size]
+                answer.append(trie.startswith(query[::-1]))
         else:
-            left_idx = bisect_left(suffix[len(q)], q[::-1].replace("?", "a"))
-            right_idx = bisect_right(suffix[len(q)], q[::-1].replace("?", "z"))
-        answer.append(right_idx - left_idx)
+            answer.append(0)
     return answer
