e1cc9bad69a26da831da918b8f11186cd655c377
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-09-01 14:26:14 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,11 +1,9 @@
-from collections import deque
-
 def solution(skill, skill_trees):
     answer = 0
     for string in skill_trees:          
-        string_list = deque(skill[:])  
-        for char in string:            
-            if char in skill and char != string_list.popleft(): 
+        stack = list(skill)[::-1]
+        for char in string:
+            if char in skill and char != stack.pop(): 
                 break
         else:
             answer += 1    
