5bdd4ab1060e950f4905f356919e2a3bb82ab917
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-09-01 12:49:31 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,35 +1,38 @@
 def solution(numbers, hand):
-    phone = {"1" : [0, 0], "2" : [0, 1], "3" : [0, 2],
-             "4" : [1, 0], "5" : [1, 1], "6" : [1, 2],
-             "7" : [2, 0], "8" : [2, 1], "9" : [2, 2],
-             "*" : [3, 0], "0" : [3, 1], "#" : [3, 2]}
-    result = []
-    cur_left = "*"
-    cur_right = "#"
+    # 키패드 위치 딕셔너리 저장
+    keypad = {1 : (0, 0), 2 : (0, 1), 3 : (0, 2),
+             4 : (1, 0), 5 : (1, 1), 6 : (1, 2),
+             7 : (2, 0), 8 : (2, 1), 9 : (2, 2),
+             "*" : (3, 0), 0 : (3, 1), "#" : (3, 2)}
+
+    # 왼손 원래 위치 + 오른손 원래 위치
+    left_pos = keypad["*"]
+    right_pos = keypad["#"]
     
+    # 번호 누른 손가락이 어느 손인지 저장하는 배열
+    answer = []
+
     for number in numbers:
-        number_str = str(number)
-        d1 = abs(phone[cur_left][0] - phone[number_str][0]) + abs(phone[cur_left][1] - phone[number_str][1])
-        d2 = abs(phone[cur_right][0] - phone[number_str][0]) + abs(phone[cur_right][1] - phone[number_str][1])
-        
-        if number_str in ["1", "4", "7"]:
-            result.append("L")
-            cur_left = number_str
-        elif number_str in ["3", "6", "9"]:
-            result.append("R")
-            cur_right = number_str
+        if number in (1, 4, 7):
+            left_pos = keypad[number]
+            answer.append("L")
+        elif number in (3, 6, 9):
+            right_pos = keypad[number]
+            answer.append("R")
         else:
-            if d1 == d2:
-                if hand == "left":
-                    result.append("L")
-                    cur_left = number_str
-                else:
-                    result.append("R")
-                    cur_right = number_str
-            elif d1 > d2:
-                result.append("R")
-                cur_right = number_str
+            ld = abs(keypad[number][0] - left_pos[0]) + abs(keypad[number][1] - left_pos[1])
+            rd = abs(keypad[number][0] - right_pos[0]) + abs(keypad[number][1] - right_pos[1])
+            if ld > rd:
+                right_pos = keypad[number]
+                answer.append("R")
+            elif ld < rd:
+                left_pos = keypad[number]
+                answer.append("L")
             else:
-                result.append("L")
-                cur_left = number_str
-    return "".join(result)
+                if hand == "left":
+                    left_pos = keypad[number]
+                    answer.append("L")
+                else:
+                    right_pos = keypad[number]
+                    answer.append("R")
+    return "".join(answer)
