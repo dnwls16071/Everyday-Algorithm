044ec0bb19bc50c1911c6f0f4477a168bb3e32ee
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-09-02 09:07:51 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164#qna) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 15일 20:52:12
+2024년 09월 02일 09:07:47
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,18 +1,21 @@
 from collections import defaultdict
 
-def solution(tickets):
-    hist = defaultdict(list)
-    for ticket in tickets:
-        a, b =  ticket
-        hist[a].append(b)
-    
-    for h in hist.keys():
-        hist[h].sort()
+def DFS(graph, path, visited):
+    if path:
+        o = path[-1]
+        if graph[o]:
+            path.append(graph[o].pop(0))
+        else:
+            visited.append(path.pop())
+        DFS(graph, path, visited)
+    return visited[::-1]
 
-    path = []
-    def DFS(start):
-        while hist[start]:
-            DFS(hist[start].pop(0))
-        path.append(start)
-    DFS("ICN")
-    return path[::-1]
+def solution(tickets):
+    graph = defaultdict(list)
+    for a, b in tickets:
+        graph[a].append(b)
+    for c in graph:
+        graph[c].sort()
+    return DFS(graph, ["ICN"], [])
+
+    
