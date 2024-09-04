4dfe3dd2ccb1e02c01aebde7e9f0bb067f5a72e0
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-09-04 11:54:51 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#qna) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 10.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 17일 23:51:35
+2024년 09월 04일 11:54:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,21 +1,20 @@
 def solution(name):
-# 두번 꺽는 경우는 고려하지 않음
-    if set(name) == {'A'}:
-        return 0
+    answer = 0
+    size = len(name)
+    min_move = size - 1
     
-    a_pos = ord('A') # 'A' : 65, 'Z' : 90
-    z_pos = ord('Z')
-    
-    answer = float('inf')
-    
-    for i in range(len(name)//2 + 1):
-        l_r = name[-i:] + name[:-i] #왼쪽먼저 갔다가 + 오른쪽
-        r_l = name[i: :-1] + name[i+1:][::-1] # 기준점에서 빠꾸 + 좌측
-        for n in [l_r,r_l]:
-            # 끝에 A들은 셀 필요 없으므로 자르기
-            while n and n[-1] == 'A':
-                n = n[:-1]
-            cnt = [min(ord(c)-a_pos, (z_pos+1) - ord(c)) for c in n ]
-            answer = min(answer, i + (len(cnt)-1) + sum(cnt))
-
+    for idx, value in enumerate(name):
+        # 알파벳을 맞추기 위해 최소로 조작해야하는 횟수
+        answer += min(ord(value) - ord('A'), ord('Z') - ord(value) + 1)
+        
+        # 조이스틱을 왼쪽으로 조작할지 오른쪽으로 조작할지
+        next_idx = idx + 1
+        while next_idx < size and name[next_idx] == 'A':
+            next_idx += 1
+        
+        # 최소로 움직이는 횟수를 반환
+        # 연속된 A를 따라서 오른쪽으로 갈 때
+        # 연속된 A를 따라가지 않고 왼쪽으로 갈 때
+        min_move = min(min_move, idx + size - next_idx + min(idx, size - next_idx))
+    answer += min_move
     return answer
