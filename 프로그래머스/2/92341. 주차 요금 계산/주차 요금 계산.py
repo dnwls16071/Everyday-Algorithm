a88c78a935190942167aa49b7a660036cf15ed58
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-09-05 14:48:59 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.63 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,34 +1,41 @@
-from collections import defaultdict
-
 def solution(fees, records):
-    answer = []
-    car_dict = {}
-    fee_dict = defaultdict(int)
-    for record in records:
-        time, car_number, io = record.split(" ")
-        time = time.split(":")
-        h = int(time[0])
-        m = int(time[1])
-
-        
-        if io == "IN":
-            car_dict[car_number] = [h, m]
-        else:
-            oh = car_dict[car_number][0]
-            om = car_dict[car_number][1]
-            pt = (h - oh) * 60 + (m - om)
-            fee_dict[car_number] += pt
-            del car_dict[car_number]
-
-    for key, value in car_dict.items():
-        fee_dict[key] += (23 - value[0]) * 60 + (59 - value[1])
     
-    for key, value in fee_dict.items():
-        dt, df, ut, uf = fees
-        if value <= dt:
-            result = df
+    def to_min(time):
+        time = time.split(':')
+        return int(time[0]) * 60 + int(time[1])
+
+    parked = dict()         # 차량 번호 : 입/출차
+    parked_time = dict()    # 차량 번호 : 주차 시간 
+    for record in records:
+        time, number, type = record.split(' ')
+        
+        if type == "IN":
+            parked[number] = to_min(time)
         else:
-            result = df + ((value - dt) // ut + int((value - dt) % ut != 0)) * uf
-        answer.append([key, result])
-    answer = sorted(answer, key = lambda x : int(x[0]))
-    return [i[1] for i in answer]
+            parked_duration = to_min(time) - parked[number]
+            if number in parked_time:
+                parked_time[number] += parked_duration
+            else:
+                parked_time[number] = parked_duration
+            del parked[number]
+    
+    for number, type in parked.items():
+        parked_duration = (23 * 60 + 59) - parked[number]
+        if number in parked_time:
+            parked_time[number] += parked_duration
+        else:
+            parked_time[number] = parked_duration
+    
+    parked_time = sorted(parked_time.items())
+    
+    result = []
+    for number, total_time in parked_time:
+        if total_time <= fees[0]:
+            result.append(fees[1])
+        else:
+            extra_time = total_time - fees[0]
+            extra_fee = (extra_time + fees[2] - 1) // fees[2] * fees[3]
+            total_fee = fees[1] + extra_fee
+            result.append(total_fee)
+    return result
+        
