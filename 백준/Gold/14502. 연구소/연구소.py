5ee5d1f197f78d8addab931ae828cd58ae0375f3
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-09-17 10:48:09 +0900

    [Gold IV] Title: 연구소, Time: 2780 ms, Memory: 34300 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,56 +1,52 @@
-from collections import deque
-from itertools import combinations
 import sys, copy
 input = sys.stdin.readline
+from collections import deque
+from itertools import combinations
 
 N, M = map(int, input().split())
-MAX =  -1   # 안전 영역 최대 크기
-graph = []  # 전체 지도 정보
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
-"""
-0 : 빈 칸
-1 : 벽
-2 : 바이러스
-"""
-virus = []      # 바이러스가 있는 좌표의 정보
-empties = []    # 빈 칸이 있는 좌표의 정보
+maps = [list(map(int, input().split())) for _ in range(N)]
+
+empty = []
+wall = []
+virus = []
 for i in range(N):
     for j in range(M):
-        if graph[i][j] == 2:
-            virus.append([i, j])
-        elif graph[i][j] == 0:
-            empties.append([i, j])
+        if maps[i][j] == 0:
+            empty.append((i, j))
+        elif maps[i][j] == 1:
+            wall.append((i, j))
+        elif maps[i][j] == 2:
+            virus.append((i, j))
 
-def BFS():
-    global MAX
-    for comb in combinations(empties, 3):
-        cg = copy.deepcopy(graph)
-        for y, x in comb:
-            cg[y][x] = 1
-        
-        cnt = 0
-        q = deque(virus)
-        while q:
-            y, x = q.popleft()
-            dy = [1, 0, -1, 0]
-            dx = [0, 1, 0, -1]
-            for i in range(4):
-                ny = y + dy[i]
-                nx = x + dx[i]
-                if ny < 0 or ny >= N or nx < 0 or nx >= M:
-                    continue            
-                if 0 <= ny < N and 0 <= nx < M:
-                    if cg[ny][nx] == 0:
-                        cg[ny][nx] = 2
-                        q.append([ny, nx])
+def BFS(a, b, cp):
+    visited = [[False] * (M + 1) for _ in range(N + 1)]
+    q = deque()
+    q.append((a, b))
+    visited[a][b] = True
+    while q:
+        dy = [1, 0, -1, 0]
+        dx = [0, 1, 0, -1]
+        y, x = q.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if ny < 0 or ny >= N or nx < 0 or nx >= M:
+                continue
+            if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+                if cp[ny][nx] == 0:
+                    cp[ny][nx] = 2
+                    visited[ny][nx] = True
+                    q.append((ny, nx))
 
-        for i in range(N):
-            for j in range(M):
-                if cg[i][j] == 0:
-                    cnt += 1
-        MAX = max(MAX, cnt)
-    return MAX
-
-result = BFS()
-print(result)
+answer = float('inf') * (-1)
+for comb in combinations(empty, 3):
+    cp = copy.deepcopy(maps)
+    for cy, cx in comb:
+        cp[cy][cx] = 1
+    for vy, vx in virus:
+        BFS(vy, vx, cp)    
+    result = 0
+    for line in cp:
+        result += line.count(0)
+    answer = max(answer, result)
+print(answer)
