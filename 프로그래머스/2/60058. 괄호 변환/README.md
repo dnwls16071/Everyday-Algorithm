14d068c6731fce67eaee321f5ee70db98b5a5bb3
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-09-17 12:25:30 +0900

    [level 2] Title: 괄호 변환, Time: 0.44 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/README.md
+++ 프로그래머스/2/60058. 괄호 변환/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 변환 - 60058 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.42 ms
+메모리: 10.2 MB, 시간: 0.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 03일 14:13:50
+2024년 09월 17일 12:25:25
 
 ### 문제 설명
 
--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,48 +1,39 @@
-def check(string):
+def check(u):
     stack = []
-    for char in string:
-        if char == "(":
-            stack.append("(")
-        elif stack:
+    for idx in range(len(u)):
+        if u[idx] == '(':
+            stack.append('(')
+        elif stack and u[idx] == ')':
             stack.pop()
         else:
             return False
-    return True
-
-def DFS(string):
-    if not string:
-        return string   #1. 입력이 빈 문자열인 경우, 빈 문자열을 반환
     
+    if stack:
+        return False
+    else:
+        return True
+
+def solution(p):
+    if len(p) == 0:
+        return ''
+
     close = 0
-    for idx in range(len(string)):
-        if string[idx] == "(":
+    for idx in range(len(p)):
+        if p[idx] == '(':
             close += 1
         else:
-            close -= 1  
+            close -= 1
         
-        #2. 문자열 w를 두 균형잡힌 괄호 문자열 u, v로 분리한다.
-        if close == 0:  
-            u = string[:idx+1]  
-            v = string[idx+1:]
-            #3. 문자열 u가 올바른 괄호 문자열이라면 문자열 v에 대해 1단계부터 다시 수행
+        if close == 0:
+            u = p[:idx+1]
+            v = p[idx+1:]
             if check(u):
-                return ''.join([u, DFS(v)])
-            #4. 문자열 u가 올바른 괄호 문자열이 아니라면 아래 과정을 수행
-            #4-1. 빈 문자열에 첫 번째 문자로 "("를 붙인다.
-            #4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과를 이어 붙인다.
-            #4-3. ")"를 붙인다.
+                return ''.join([u, solution(v)])
             else:
-                empty = ["(", DFS(v), ")"]
-                #4-4. u의 첫 번째 문자와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙인다.
-                for t in range(1, idx):
-                    if u[t] == "(":
-                        empty.append(")")
+                result = ['(', solution(v), ')']
+                for i in u[1:-1]:
+                    if i == '(':
+                        result.append(')')
                     else:
-                        empty.append("(")
-                return "".join(empty)
-                    
-def solution(p):
-    return DFS(p)
-    
-    
-            
+                        result.append('(')
+                return ''.join(map(str, result))
