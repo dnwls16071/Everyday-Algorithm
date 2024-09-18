1fb0471215447e5c4c02bbe519d6bd9ade0e380a
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-09-18 16:02:40 +0900

    [Gold V] Title: 감시 피하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,75 +1,76 @@
-import sys, copy
+import sys
 input = sys.stdin.readline
 from itertools import combinations
 
+# 선생님의 감시(일직선 방향으로)    
+def search(y, x, direction):
+    if direction == 0:
+        while y >= 0:
+            if maps[y][x] == "S":
+                return False
+            if maps[y][x] == "O":
+                return True
+            y -= 1
+    elif direction == 1:
+        while x >= 0:
+            if maps[y][x] == "S":
+                return False
+            if maps[y][x] == "O":
+                return True
+            x -= 1
+    elif direction == 2:
+        while y < N:
+            if maps[y][x] == "S":
+                return False
+            if maps[y][x] == "O":
+                return True
+            y += 1
+    elif direction == 3:
+        while x < N:
+            if maps[y][x] == "S":
+                return False
+            if maps[y][x] == "O":
+                return True
+            x += 1
+    return True
+            
+def process():
+    for ty, tx in teacher:
+        for i in range(4):
+            if not search(ty, tx, i):
+                return False
+    return True
+
+# S : 학생, T : 선생님, O : 장애물
+# 최대 : 36
+# 전체 선생님의 수 5이하
+# 전체 학생 수 30이하
 N = int(input())
-graph = [list(map(str, input().split())) for _ in range(N)]
+maps = [list(map(str, input().split())) for _ in range(N)]
 
+student = []
 teacher = []
-obstacle = []
-
+empty = []
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == "T":
+        if maps[i][j] == "S":
+            student.append((i, j))
+        elif maps[i][j] == "T":
             teacher.append((i, j))
-        elif graph[i][j] == "X":
-            obstacle.append((i, j))
+        elif maps[i][j] == "X":
+            empty.append((i, j))
 
-def search(y, x, direction, graph):
-    # 위쪽 검사
-    if direction == 0:
-        while y > -1:
-            if graph[y][x] == "S":
-                return True
-            if graph[y][x] == "O":
-                return False
-            y -= 1
-    # 오른쪽 검사
-    if direction == 1:
-        while x < N:
-            if graph[y][x] == "S":
-                return True
-            if graph[y][x] == "O":
-                return False
-            x += 1
-    # 아래 검사
-    if direction == 2:
-        while y < N:
-            if graph[y][x] == "S":
-                return True
-            if graph[y][x] == "O":
-                return False
-            y += 1
-    # 왼쪽 검사
-    if direction == 3:
-        while x > -1:
-            if graph[y][x] == "S":
-                return True
-            if graph[y][x] == "O":
-                return False
-            x -= 1
-    return False
-
-# 선생님 기준으로 상하좌우 방향 일직선 탐색
-def linear(board):
-    for i in teacher:
-        ty, tx = i
-        for i in range(4):
-            if search(ty, tx, i, board):
-                return True
-    return False
-
-find = False    # 애들이 전부 감시로부터 피할 수 있는지
-for comb in combinations(obstacle, 3):
-    temp = copy.deepcopy(graph)
-    for y, x in comb:
-        temp[y][x] = "O"
-    
-    if not linear(temp):
-        find = True 
+flag = False
+for comb in combinations(empty, 3):
+    for cy, cx in comb:
+        maps[cy][cx] = "O"
+    if process():
+        flag = True
         break
+    for cy, cx in comb:
+        maps[cy][cx] = "X"
 
-if find:
+if flag:
     print("YES")
 else:
     print("NO")
