fd248f14c8d4947ee6d3f5396df10a60237f2e88
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-09-21 11:57:08 +0900

    [Silver II] Title: 병사 배치하기, Time: 692 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -2,11 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst = list(map(int, input().strip().split()))
-dp = [1] * (N+1)
+data = list(map(int, input().split()))
+
+dp = [1] * (N + 1)  # 병사 전투 배치 현황
 
 for i in range(1, N):
     for j in range(i):
-        if lst[i] < lst[j]:
+        if data[i] < data[j]:
             dp[i] = max(dp[i], dp[j] + 1)
+
 print(N - max(dp))
