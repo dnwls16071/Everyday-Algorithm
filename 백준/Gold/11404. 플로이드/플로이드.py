f6f456767e49340fc0241d2998f38fafc7cd2774
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-09-24 21:30:57 +0900

    [Gold IV] Title: 플로이드, Time: 592 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,27 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-INF = int(1e7)
-N = int(input())
-M = int(input())
-graph = [[INF] * (N+1) for _ in range(N+1)]
-
-for _ in range(M):
+n = int(input())
+m = int(input())
+inf = float('inf')
+graph = [[inf] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = min(graph[a][b], c)
 
-for i in range(1, N+1):
-    graph[i][i] = 0
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            if a == b:
+                continue
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-for k in range(1, N+1):
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+for i in range(n+1):
+    for j in range(n+1):
+        if graph[i][j] == inf:
+            graph[i][j] = 0
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if graph[i][j] == INF:
-            print(0, end=" ")
-        else:
-            print(graph[i][j], end=" ")
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(graph[i][j], end=" ")
     print()
