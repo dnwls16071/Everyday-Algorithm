90b5a520fb9f1d63c2ec5e2ed5dd1c99514c6ec1
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-09-28 16:30:20 +0900

    [Silver III] Title: 소수 구하기, Time: 240 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -2,14 +2,15 @@ import sys
 input = sys.stdin.readline
 
 M, N = map(int, input().split())
-array = [True] * (N+1)
-array[1] = False
+number = [True] * (N + 1)
+number[0] = False
+number[1] = False
 
 for i in range(2, int(N**0.5)+1):
-    if array[i]:
-        for j in range(i*i, N+1, i):
-            array[j] = False
+    if number[i]:
+        for j in range(2*i, N+1, i):
+            number[j] = False
 
 for i in range(M, N+1):
-    if array[i]:
+    if number[i]:
         print(i)
