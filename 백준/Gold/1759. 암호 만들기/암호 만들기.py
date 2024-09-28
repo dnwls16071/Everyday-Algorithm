e20ef83352541f238b29382ceeb99a2dd5dab092
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-09-28 16:43:18 +0900

    [Gold V] Title: 암호 만들기, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,24 +1,20 @@
-import sys
+import sys, itertools
 input = sys.stdin.readline
-from itertools import combinations
 
 L, C = map(int, input().split())
-lst = ['a', 'e', 'i', 'o', 'u']
-array = list(map(str, input().split()))
-array.sort()
+lst = list(map(str, input().split()))
+lst.sort()
 
 result = []
-for comb in combinations(array, L):
-    vowel_cnt = 0
-    consonant_cnt = 0
-    for i in comb:
-        if i in lst:
-            vowel_cnt += 1
+for comb in itertools.combinations(lst, L):
+    vowel, constant = 0, 0
+    for c in comb:
+        if c in ['a', 'e', 'i', 'o', 'u']:
+            vowel += 1
         else:
-            consonant_cnt += 1
-    if vowel_cnt >= 1 and consonant_cnt >= 2:
-        result.append("".join(map(str, comb)))
+            constant += 1
+    if vowel >= 1 and constant >= 2:
+        result.append(''.join(map(str, comb)))
 
-result.sort()
-for i in result:
-    print(i)
+for r in result:
+    print(r)
