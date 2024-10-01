5a83862b1ea5df8af9fee1c627f6d1dd4fb4f8cf
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2024-10-01 17:30:44 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 305.96 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -1,23 +1,28 @@
+# 우선순위 : 음식 번호
+# 선형 탐색 : O(N)
+# 최소 힙 : O(N log N)
 import heapq
 
 def solution(food_times, k):
+    heap = []
+    for idx, food in enumerate(food_times):
+        heapq.heappush(heap, (food, idx))
+    
+    
     if sum(food_times) <= k:
         return -1
-
-    q = []
-    for idx, time in enumerate(food_times):
-        # 최소힙 구성
-        heapq.heappush(q, (time, idx + 1))
     
-    sum_times = 0
-    previous_times = 0
-    length = len(food_times)
-    
-    while sum_times + (q[0][0] - previous_times) * length <= k:
-        now_times = heapq.heappop(q)[0]
-        sum_times += (now_times - previous_times) * length
+    # 계산 부분
+    # k초 이내의 범위에서는 무한 반복
+    tt = 0  # 전체 시간
+    nt = 0  # 현재 시간
+    pt = 0  # 이전 시간
+    length = len(heap)
+    while tt + (heap[0][0] - pt) * length <= k:
+        nt = heapq.heappop(heap)[0]
+        tt += (nt - pt) * length
         length -= 1
-        previous_times = now_times
-    
-    result = sorted(q, key=lambda x : x[1])
-    return result[(k - sum_times) % length][1]
+        pt = nt 
+        
+    result = sorted(heap, key=lambda x : x[1])
+    return result[(k - tt) % length][1] + 1
